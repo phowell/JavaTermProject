98447b4fb2e31c839e66ee0231c2dffc8a55364b
Andrew Howell <andrew.t.howell@gmail.com>
2022-08-03 20:05:15 -0230

    Second commit. Change made.

--- src/Game.java
+++ src/Game.java
@@ -5,5 +5,6 @@ This class will be the main class for the game logic and will contain main()
 public class Game {
     public static void main(String[] args) {
 	System.out.println("Yoooooo");
+	System.out.println("GITHUBBBBBBBB");
     }
 }
